a659284d2db98d6672fdb5a5ff298a89a6b1c969
Hassaan Zahid <hassaanzahid28@gmail.com>
2026-04-11 23:46:17 +0500

    dr image updated

--- src/pages/Doctor.tsx
+++ src/pages/Doctor.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Phone, Mail, Award, BookOpen, Briefcase, Heart, MessageCircleHeartIcon } from "lucide-react";
 import SEOHead from "@/components/SEOHead";
-import doctorImg from "@/assets/doctor-placeholder.webp";
+import doctorImg from "@/assets/doctor.png";
 
 const DoctorPage = () => {
   useEffect(() => {
